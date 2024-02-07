b10741f996c2fb7ccd73ee69b98ebf215a29205b
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2024-02-07 15:08:02 -0800

    Update thresholdByFDR.sh

--- thresholdByFDR.sh
+++ thresholdByFDR.sh
@@ -1,2 +1,8 @@
-result_file=Tos_cis_eQTLs_1gPCs_quantile_10ePCs_lab_sex
+ls | grep -v '\.png$' | grep 'cis_eQTL' > all_result_files.txt
+
 awk -v bash_value="0.05" '{ if ($6 <= bash_value) print }' ${result_file} > ${file}_0.05_FDR
+
+
+while IFS= read -r line; do
+    awk -v bash_value="0.05" '{ if ($6 <= bash_value) print }' $line > ${line}_0.05_FDR
+done < all_result_files.txt

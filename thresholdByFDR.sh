619c571891c58a84a211cb5e1fc3ab99fbc819bf
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2024-02-07 15:01:07 -0800

    Update thresholdByFDR.sh

--- thresholdByFDR.sh
+++ thresholdByFDR.sh
@@ -1,2 +1,2 @@
 result_file=Tos_cis_eQTLs_1gPCs_quantile_10ePCs_lab_sex
-awk -v bash_value="0.05" '{ if ($6 <= bash_value) print }' ${result_file} > file2.txt
+awk -v bash_value="0.05" '{ if ($6 <= bash_value) print }' ${result_file} > ${file}_0.05_FDR

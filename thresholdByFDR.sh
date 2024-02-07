f95cce38fe2ab167ca91585b923cef52f954ae05
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2024-02-07 15:10:07 -0800

    Update thresholdByFDR.sh

--- thresholdByFDR.sh
+++ thresholdByFDR.sh
@@ -2,7 +2,6 @@ ls | grep -v '\.png$' | grep 'cis_eQTL' > all_result_files.txt
 
 awk -v bash_value="0.05" '{ if ($6 <= bash_value) print }' ${result_file} > ${file}_0.05_FDR
 
-
 while IFS= read -r line; do
     awk -v bash_value="0.05" '{ if ($6 <= bash_value) print }' $line > ${line}_0.05_FDR
 done < all_result_files.txt
